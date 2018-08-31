582e6f799d94324891c32b3d36ba6436e2e657ad
XiaoOOMing <13261675306@163.com>
2018-08-31 15:48:18 +0800

    home adv finished

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-adv></home-adv>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/HomeIcons'
+import HomeAdv from './components/HomeAdv'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeAdv
   }
 }
 </script>
---
+++ src/pages/home/components/HomeAdv.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home-adv">
+    <div class="item border-right">
+      <img src="http://img1.qunarzz.com/piao/fusion/1806/29/30427c0e0658b502.png" class="item-image">
+    </div>
+    <div class="item">
+      <img src="http://img1.qunarzz.com/piao/fusion/1808/84/887ec92cae51f302.png" class="item-image">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeAdv'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~@css/variable.styl"
+  .home-adv
+    margin-top $marginTop
+    background #fff
+    display flex
+    .item
+      width 50%
+      .item-image
+        width 100%
+</style>
