5d062b0542f170cc9aa2bc0f5f151f2070afe866
XiaoOOMing <13261675306@163.com>
2018-08-31 15:05:33 +0800

    home swiper finished

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,15 +1,68 @@
 <template>
-  <div class="swiper">
-    swiper home
-  </div>
+  <swiper :options="swiperOption" class="swiper">
+    <!-- slides -->
+    <swiper-slide v-for="item of swiperList" :key="item.id" class="swiper-container">
+      <img :src="item.imgUrl" class="swiper-image">
+    </swiper-slide>
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
 </template>
 
 <script>
 export default {
-  name: 'Swiper'
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 5000
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/b0/a123fba6d7e3dd02.jpg_750x200_1425741a.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/8a/1957a0fb58ad0402.jpg_750x200_e72b8c0f.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/f4/5f2289f8675f0502.jpg_750x200_ab1633c7.jpg'
+        },
+        {
+          id: '0004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/bd/8e4a1c3f470d3702.jpg_750x200_f1f0a8c7.jpg'
+        },
+        {
+          id: '0005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/18/2dbce82b07a23402.jpg_750x200_14356b03.jpg'
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+  .swiper >>> .swiper-pagination-bullets
+    bottom .1rem
+  .swiper >>> .swiper-pagination-bullet
+    width .12rem
+    height .12rem
+    background #fff
+    opacity .4
+    margin 0 .08rem
+  .swiper >>> .swiper-pagination-bullet-active
+    background #fff
+    opacity 1
+  .swiper-container
+    width 100%
+    height 0
+    padding-bottom 26.7%
+    overflow hidden
+    .swiper-image
+      width 100%
 </style>
