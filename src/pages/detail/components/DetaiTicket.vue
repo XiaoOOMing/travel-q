bf029d271dd608129817dd99253ea3ae1de7c367
XiaoOOMing <13261675306@163.com>
2018-09-21 16:46:20 +0800

    detail ticket list ok

--- src/pages/detail/components/DetaiTicket.vue
+++ src/pages/detail/components/DetaiTicket.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="d-ticket">
+  <div class="d-ticket" ref="tagTop">
     <div class="tag border-bottom">
-      <div class="item active">门票</div>
-      <a href="#playOneDay" class="item">一日游</a>
+      <div class="item" :class="{active: tag}" ref="ticket">门票</div>
+      <div class="item" :class="{active: !tag}" ref="oneDay">一日游</div>
     </div>
     <div class="ticket-item">
       <div class="head-title border-bottom">
@@ -82,7 +82,8 @@
         </div>
       </div>
     </div>
-    <div class="ticket-item" id="playOneDay">
+    <div ref="oneDayAnchor"></div>
+    <div class="ticket-item">
       <div class="head-title border-bottom">
         <i class="iconfont">&#xe659;</i>
         <span>一日游</span>
@@ -103,17 +104,67 @@
 
 <script>
 export default {
-  name: 'DetaiTicket'
+  name: 'DetaiTicket',
+  data () {
+    return {
+      tag: true
+    }
+  },
+  mounted () {
+    this.bindEvents()
+    window.addEventListener('scroll', this.tagPosition)
+  },
+  beforeDestroy () {
+    window.removeEventListener('scroll', this.tagPosition)
+  },
+  methods: {
+    // DOM事件绑定
+    bindEvents () {
+      const _this = this
+      // 点击一日游
+      this.$refs.oneDay.onclick = function () {
+        let scrollTop = _this.$refs.oneDayAnchor.offsetTop - 88
+        window.scrollTo(0, scrollTop)
+        _this.tag = false
+      }
+      // 点击门票
+      this.$refs.ticket.onclick = function () {
+        let scrollTop = _this.$refs.tagTop.offsetTop - 44
+        window.scrollTo(0, scrollTop)
+        _this.tag = true
+      }
+    },
+    // 门票 一日游固定在顶部
+    tagPosition () {
+      let tagTop = this.$refs.tagTop.offsetTop - 44
+      let scrollTop = document.body.scrollTop === 0 ? document.documentElement.scrollTop : document.body.scrollTop
+      if (tagTop < scrollTop) {
+        this.$refs.tagTop.classList.add('fixed')
+      } else {
+        this.$refs.tagTop.classList.remove('fixed')
+      }
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import "~@css/variable.styl"
+  html, body
+    transition all .3s
   .d-ticket
     margin-top .2rem
+    &.fixed
+      padding-top .88rem
+      .tag
+        position fixed
+        z-index 99
+        background-color #fff
+        top .88rem
     .tag
       background #fff
       display flex
+      width 100%
       .item
         position relative
         text-align center
